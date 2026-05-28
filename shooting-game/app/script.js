0a20060f1be4af97fc10f58781d263d1bff87569
ahmad-ki <156342918+ahmad-ki@users.noreply.github.com>
2026-05-28 13:19:10 +0400

    Shooting game

--- shooting-game/app/script.js
+++ shooting-game/app/script.js
@@ -2,6 +2,7 @@ let score = 0;
 const target = document.getElementById("target");
 const scoreDisplay = document.getElementById("score");
 
+// Move target randomly
 function moveTarget() {
   const x = Math.random() * 350;
   const y = Math.random() * 350;
@@ -9,10 +10,22 @@ function moveTarget() {
   target.style.top = y + "px";
 }
 
+// Click event
 target.onclick = function () {
   score++;
   scoreDisplay.innerText = score;
   moveTarget();
 };
 
-setInterval(moveTarget, 1000);
+// Move automatically
+setInterval(moveTarget, 800);
+
+// Show pod hostname
+fetch("/hostname")
+  .then(res => res.text())
+  .then(data => {
+    document.getElementById("pod").innerText = data;
+  })
+  .catch(() => {
+    document.getElementById("pod").innerText = "unknown";
+  });
